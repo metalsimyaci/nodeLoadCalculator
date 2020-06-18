224e92887ddd4b2a3439570f31f3a3cdfe54ac0e
Hasan URAL <metalsimyaci@outlook.com>
2020-06-18 23:43:54 +0300

    Complated

---
+++ src/NodeLoadCalculator/NodeLoadCalculator.Desktop/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace NodeLoadCalculator.Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.grp_values = new System.Windows.Forms.GroupBox();
+            this.flp_values = new System.Windows.Forms.FlowLayoutPanel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.grp_relations = new System.Windows.Forms.GroupBox();
+            this.flp_relations = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_result = new System.Windows.Forms.Panel();
+            this.grp_result = new System.Windows.Forms.GroupBox();
+            this.flp_result = new System.Windows.Forms.FlowLayoutPanel();
+            this.grp_values.SuspendLayout();
+            this.grp_relations.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.pnl_result.SuspendLayout();
+            this.grp_result.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(10, 919);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(195, 63);
+            this.btn_calculate.TabIndex = 0;
+            this.btn_calculate.Text = "Hesapla";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // grp_values
+            // 
+            this.grp_values.Controls.Add(this.flp_values);
+            this.grp_values.Dock = System.Windows.Forms.DockStyle.Left;
+            this.grp_values.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grp_values.Location = new System.Drawing.Point(0, 0);
+            this.grp_values.Name = "grp_values";
+            this.grp_values.Padding = new System.Windows.Forms.Padding(10);
+            this.grp_values.Size = new System.Drawing.Size(362, 466);
+            this.grp_values.TabIndex = 1;
+            this.grp_values.TabStop = false;
+            this.grp_values.Text = "Degerler";
+            // 
+            // flp_values
+            // 
+            this.flp_values.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp_values.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.flp_values.Location = new System.Drawing.Point(10, 38);
+            this.flp_values.Name = "flp_values";
+            this.flp_values.Size = new System.Drawing.Size(342, 418);
+            this.flp_values.TabIndex = 0;
+            // 
+            // grp_relations
+            // 
+            this.grp_relations.Controls.Add(this.flp_relations);
+            this.grp_relations.Dock = System.Windows.Forms.DockStyle.Right;
+            this.grp_relations.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grp_relations.Location = new System.Drawing.Point(636, 0);
+            this.grp_relations.Name = "grp_relations";
+            this.grp_relations.Padding = new System.Windows.Forms.Padding(10);
+            this.grp_relations.Size = new System.Drawing.Size(533, 466);
+            this.grp_relations.TabIndex = 2;
+            this.grp_relations.TabStop = false;
+            this.grp_relations.Text = "Iliskiler";
+            // 
+            // flp_relations
+            // 
+            this.flp_relations.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp_relations.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.flp_relations.Location = new System.Drawing.Point(10, 38);
+            this.flp_relations.Name = "flp_relations";
+            this.flp_relations.Size = new System.Drawing.Size(513, 418);
+            this.flp_relations.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.grp_values);
+            this.panel1.Controls.Add(this.grp_relations);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(10, 238);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1169, 466);
+            this.panel1.TabIndex = 3;
+            // 
+            // pnl_result
+            // 
+            this.pnl_result.Controls.Add(this.grp_result);
+            this.pnl_result.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_result.Location = new System.Drawing.Point(10, 10);
+            this.pnl_result.Name = "pnl_result";
+            this.pnl_result.Size = new System.Drawing.Size(1169, 228);
+            this.pnl_result.TabIndex = 4;
+            // 
+            // grp_result
+            // 
+            this.grp_result.Controls.Add(this.flp_result);
+            this.grp_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grp_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.grp_result.Location = new System.Drawing.Point(0, 0);
+            this.grp_result.Name = "grp_result";
+            this.grp_result.Padding = new System.Windows.Forms.Padding(10);
+            this.grp_result.Size = new System.Drawing.Size(1169, 228);
+            this.grp_result.TabIndex = 2;
+            this.grp_result.TabStop = false;
+            this.grp_result.Text = "Dugum Puanlari";
+            // 
+            // flp_result
+            // 
+            this.flp_result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.flp_result.Location = new System.Drawing.Point(10, 38);
+            this.flp_result.Name = "flp_result";
+            this.flp_result.Size = new System.Drawing.Size(1149, 180);
+            this.flp_result.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 995);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_result);
+            this.Controls.Add(this.btn_calculate);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dugum Yuk Hesaplayici";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grp_values.ResumeLayout(false);
+            this.grp_relations.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.pnl_result.ResumeLayout(false);
+            this.grp_result.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.GroupBox grp_values;
+        private System.Windows.Forms.FlowLayoutPanel flp_values;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox grp_relations;
+        private System.Windows.Forms.FlowLayoutPanel flp_relations;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_result;
+        private System.Windows.Forms.GroupBox grp_result;
+        private System.Windows.Forms.FlowLayoutPanel flp_result;
+    }
+}
+
---
+++ src/NodeLoadCalculator/NodeLoadCalculator.Desktop/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NodeLoadCalculator.Desktop
+{
+    public partial class Form1 : Form
+    {
+        #region Constants
+        const string valueFileName = "data.txt";
+        const string relationFileName = "data2.txt";
+        const string sparatorCharacter = " ";
+        const int padTextLength = 3;
+        #endregion
+
+        #region Variables
+        static int[] values;//Deger
+        static int[,] relation;//baglant
+        #endregion
+
+        #region Preparing Methods
+        private static int[] splitterValue(string rawString)
+        {
+            var splittedString = rawString.Split(new string[] { sparatorCharacter }, StringSplitOptions.RemoveEmptyEntries);
+            var node = new int[splittedString.Length];
+            var index = 0;
+            foreach (var s in splittedString)
+            {
+                if (int.TryParse(s, out var value))
+                {
+                    node[index] = value;
+                    index++;
+                }
+            }
+
+            return node;
+        }
+        private static int[,] splitterRelation(string rawString)
+        {
+
+            if (string.IsNullOrEmpty(rawString))
+                return null;
+
+            if (rawString.IndexOf("\n") > -1)
+            {
+                var lines = rawString.Split(new string[] { "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+                var relations = new int[lines.Length, lines.Length];
+                var x = 0;
+                foreach (var line in lines)
+                {
+
+                    var splittedString = line.Split(new string[] { sparatorCharacter }, StringSplitOptions.RemoveEmptyEntries);
+                    var y = 0;
+                    foreach (var s in splittedString)
+                    {
+                        if (int.TryParse(s, out var value))
+                        {
+                            relations[x, y] = value;
+                            y++;
+                        }
+                    }
+                    x++;
+                }
+
+                return relations;
+            }
+            return null;
+        }
+        public static void ReadFileValue()
+        {
+
+            var valueFilePath = Path.Combine(Environment.CurrentDirectory, valueFileName);
+            using (StreamReader reader = new StreamReader(valueFilePath))
+            {
+                var line = reader.ReadLine();
+                values = splitterValue(line);
+                reader.Close();
+
+            }
+        }
+        public static void ReadFileRelation()
+        {
+
+            var relationFilePath = Path.Combine(Environment.CurrentDirectory, relationFileName);
+            using (StreamReader reader = new StreamReader(relationFilePath))
+            {
+                string rawContent = reader.ReadToEnd();
+                relation = splitterRelation(rawContent);
+                reader.Close();
+            }
+        }
+        private Panel CreateNode(string label, string val, string name)
+        {
+            var tb = new TextBox();
+            tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            tb.Location = new System.Drawing.Point(0, 25);
+            tb.Name = $"tb_{name}";
+            tb.Size = new System.Drawing.Size(30, 26);
+            tb.TabIndex = 0;
+            tb.Enabled = false;
+
+            tb.Text = val;
+
+            var lb = new Label();
+            lb.AutoSize = true;
+            lb.Dock = System.Windows.Forms.DockStyle.Top;
+            lb.Location = new System.Drawing.Point(0, 5);
+            lb.Name = $"lb_{name}";
+            lb.Size = new System.Drawing.Size(30, 20);
+            lb.TabIndex = 0;
+            lb.Text = label;
+
+            var pnl = new Panel();
+            pnl.Controls.Add(tb);
+            pnl.Controls.Add(lb);
+            pnl.Name = $"pnl_{name}";
+            pnl.Size = new System.Drawing.Size(30, 57);
+            pnl.TabIndex = 3;
+
+            return pnl;
+        }
+        private Label CreateLabel(string label, string name)
+        {
+            var lb = new Label();
+            lb.AutoSize = true;
+            lb.Dock = System.Windows.Forms.DockStyle.Top;
+            lb.Location = new System.Drawing.Point(0, 5);
+            lb.Name = $"lb_{name}";
+            lb.Size = new System.Drawing.Size(30, 20);
+            lb.TabIndex = 0;
+            lb.Text = label;
+
+            return lb;
+        }
+        private void DrawValues()
+        {
+            var counter = 1;
+            this.flp_values.Controls.Clear();
+            foreach (var i in values)
+            {
+                var p = CreateNode(counter.ToString(), i.ToString(), $"vls{counter}");
+
+                this.flp_values.Controls.Add(p);
+                counter++;
+            }
+        }
+        private void DrawRelations()
+        {
+            this.flp_relations.Controls.Clear();
+            for (int i = 0; i <= relation.GetLength(0); i++)
+            {
+                for (int j = 0; j <= relation.GetLength(1); j++)
+                {
+                    Label l;
+
+                    if (i == 0 && j == 0)
+                        l = CreateLabel($"X".PadRight(3, ' '), $"rlt{i}{j}");
+                    else if (i == 0)
+                        l = CreateLabel($"{j}".PadRight(3, ' '), $"rlt{i}{j}");
+                    else if (j == 0)
+                        l = CreateLabel($"{i}".PadRight(3, ' '), $"rlt{i}{j}");
+                    else
+                        l = CreateLabel($"{relation[i - 1, j - 1]}".PadRight(3, ' '), $"rlt{i}{j}");
+
+
+
+                    this.flp_relations.Controls.Add(l);
+                }
+            }
+        }
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Calculate(int node, List<int> searchedNode)
+        {
+            if (searchedNode == null)
+                searchedNode = new List<int>();
+            else if (searchedNode.Any(x => x == node))
+                return 0;
+
+            searchedNode.Add(node);
+            int total = values[node];
+            for (int i = 0; i < relation.GetLength(1); i++)
+            {
+                if (relation[node, i] == 1)
+                    total += Calculate(i, searchedNode);
+            }
+            return total;
+        }
+        private void DrawNodePoint()
+        {
+            for(int i=0; i<values.Length; i++) {
+                var val = Calculate(i, null);
+                var p = CreateNode($"{i + 1}", $"{val}", $"np{i}");
+                flp_result.Controls.Add(p);
+            }
+        }
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            ReadFileValue();
+            DrawValues();
+            ReadFileRelation();
+            DrawRelations();
+
+            DrawNodePoint();
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
